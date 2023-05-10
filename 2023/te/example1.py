48018e785757d2f5d57d561967d1359bbcd5cc9f
Xavier Dupre <xadupre@microsoft.com>
2023-05-10 14:35:13 +0000

    add examples

---
+++ 2023/te/example1.py
@@ -0,0 +1,452 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Getting Started
+# 
+# ## Overview
+# 
+# Transformer Engine (TE) is a library for accelerating Transformer models on NVIDIA GPUs, providing better performance with lower memory utilization in both training and inference. It provides support for 8-bit floating point (FP8) precision on Hopper GPUs, implements a collection of highly optimized building blocks for popular Transformer architectures, and exposes an automatic-mixed-precision-like API that can be used seamlessy with your PyTorch code. It also includes a framework-agnostic C++ API that can be integrated with other deep learning libraries to enable FP8 support for Transformers.
+# 
+# ## Let's build a Transformer layer!
+# 
+# <div class="alert alert-info">
+# 
+# <b>Summary</b>
+#     
+# We build a basic Transformer layer using regular PyTorch modules. This will be our baseline for later comparisons with Transformer Engine.
+# 
+# </div>
+# 
+# Let's start with creating a GPT encoder layer using plain PyTorch. Figure 1 shows the overall structure.
+# 
+# <figure align="center">
+# <img src="transformer_layer.png" width="20%">
+# <figcaption> Figure 1: Structure of a GPT encoder layer.</figcaption>
+# </figure>
+# 
+# We construct the components as follows:
+# 
+# - `LayerNorm`: `torch.nn.LayerNorm`
+# - `QKV Projection`: `torch.nn.Linear` (conceptually three `Linear` layers for Q, K, and V separately, but we fuse into a single `Linear` layer that is three times larger)
+# - `DotProductAttention`: `DotProductAttention` from [quickstart_utils.py](quickstart_utils.py)
+# - `Projection`: `torch.nn.Linear`
+# - `Dropout`: `torch.nn.Dropout`
+# - `MLP`: `BasicMLP` from [quickstart_utils.py](quickstart_utils.py)
+# 
+# Over the course of this tutorial we will use a few modules and helper functions defined in [quickstart_utils.py](quickstart_utils.py). Putting it all together:
+
+# In[1]:
+
+
+import torch
+import quickstart_utils as utils
+
+class BasicTransformerLayer(torch.nn.Module):
+    def __init__(
+        self,
+        hidden_size: int,
+        ffn_hidden_size: int,
+        num_attention_heads: int,
+        layernorm_eps: int = 1e-5,
+        attention_dropout: float = 0.1,
+        hidden_dropout: float = 0.1,
+    ):
+        super().__init__()
+        self.num_attention_heads = num_attention_heads
+        self.kv_channels = hidden_size // num_attention_heads
+        self.ln1 = torch.nn.LayerNorm(hidden_size, eps=layernorm_eps)
+        self.qkv_projection = torch.nn.Linear(hidden_size, 3 * hidden_size, bias=True)
+        self.attention = utils.DotProductAttention(
+            num_attention_heads=num_attention_heads,
+            kv_channels=self.kv_channels,
+            attention_dropout=attention_dropout,
+        )
+        self.projection = torch.nn.Linear(hidden_size, hidden_size, bias=True)
+        self.dropout = torch.nn.Dropout(hidden_dropout)
+        self.ln2 = torch.nn.LayerNorm(hidden_size, eps=layernorm_eps)
+        self.mlp = utils.BasicMLP(
+            hidden_size=hidden_size,
+            ffn_hidden_size=ffn_hidden_size,
+        ) 
+        
+    def forward(
+        self, 
+        x: torch.Tensor, 
+        attention_mask: torch.Tensor = None
+    ) -> torch.Tensor:
+        res = x
+        x = self.ln1(x)
+        
+        # Fused QKV projection
+        qkv = self.qkv_projection(x)
+        qkv = qkv.view(qkv.size(0), qkv.size(1), self.num_attention_heads, 3 * self.kv_channels)
+        q, k, v = torch.split(qkv, qkv.size(3) // 3, dim=3)
+        
+        x = self.attention(q, k, v, attention_mask)
+        x = self.projection(x)
+        x = self.dropout(x)
+        x = res + x
+        res = x
+        x = self.ln2(x)
+        x = self.mlp(x)
+        
+        return x + res
+
+
+# That's it! We now have a simple Transformer layer. We can test it:
+
+# In[2]:
+
+
+# Layer configuration
+hidden_size = 128
+sequence_length = 64
+batch_size = 4
+ffn_hidden_size = 512
+num_attention_heads = 32
+dtype = torch.float16
+
+# Synthetic data
+x = torch.rand(sequence_length, batch_size, hidden_size).cuda().to(dtype=dtype)
+dy = torch.rand(sequence_length, batch_size, hidden_size).cuda().to(dtype=dtype)
+
+
+# In[3]:
+
+
+basic_transformer = BasicTransformerLayer(
+    hidden_size,
+    ffn_hidden_size,
+    num_attention_heads,
+)
+basic_transformer.to(dtype=dtype).cuda()
+
+
+# In[4]:
+
+
+torch.manual_seed(1234)
+y = basic_transformer(x, attention_mask=None)
+
+
+# In[5]:
+
+
+fprof = lambda: utils.speedometer_simple(
+    basic_transformer,
+    x,
+    dy,
+    forward_kwargs = { "attention_mask": None },
+)
+fprof()
+
+
+# In[6]:
+
+
+from torch.onnx import export
+export(basic_transformer, x, "basic.onnx", verbose=False, input_names=["X"], output_names=["Y"])
+
+
+# In[7]:
+
+
+from pyquickhelper.pycode.profiling import profile
+from pyquickhelper.pycode.profiling import profile2graph
+stat, text = profile(fprof)
+
+gr = profile2graph(stat)
+print(gr[0].to_text(fct_width=80))
+
+
+# ## Meet Transformer Engine
+# 
+# <div class="alert alert-info">
+# 
+# <b>Summary</b>
+#     
+# We modify the example Transformer layer to include the simplest TE modules: `Linear` and `LayerNorm`.
+# 
+# </div>
+# 
+# Now that we have a basic Transformer layer, let's use Transformer Engine to speed up the training. 
+
+# In[8]:
+
+
+import transformer_engine.pytorch as te
+
+
+# TE provides a set of PyTorch modules that can be used to build Transformer layers. The simplest of the provided modules are the `Linear` and `LayerNorm` layers, which we can use instead of `torch.nn.Linear` and `torch.nn.LayerNorm`. Let's modify `BasicTransformerLayer`:
+
+# In[14]:
+
+
+class BasicTEMLP(torch.nn.Module):
+    def __init__(self,
+                 hidden_size: int,
+                 ffn_hidden_size: int) -> None:
+        super().__init__()
+        self.linear1 = te.Linear(hidden_size, ffn_hidden_size, bias=True)
+        self.linear2 = te.Linear(ffn_hidden_size, hidden_size, bias=True)
+
+    def forward(self, x):
+        x = self.linear1(x)
+        x = torch.nn.functional.gelu(x, approximate='tanh')
+        x = self.linear2(x)
+        return x    
+    
+class BasicTETransformerLayer(torch.nn.Module):
+    def __init__(self,
+                 hidden_size: int,
+                 ffn_hidden_size: int,
+                 num_attention_heads: int,
+                 layernorm_eps: int = 1e-5,
+                 attention_dropout: float = 0.1,
+                 hidden_dropout: float = 0.1):
+        super().__init__()
+        self.num_attention_heads = num_attention_heads
+        self.kv_channels = hidden_size // num_attention_heads
+        self.ln1 = te.LayerNorm(hidden_size, eps=layernorm_eps)
+        self.qkv_projection = te.Linear(hidden_size, 3 * hidden_size, bias=True)
+        self.attention = utils.DotProductAttention(
+            num_attention_heads=num_attention_heads,
+            kv_channels=self.kv_channels,
+            attention_dropout=attention_dropout,
+        )
+        self.projection = te.Linear(hidden_size, hidden_size, bias=True)
+        self.dropout = torch.nn.Dropout(hidden_dropout)
+        self.ln2 = te.LayerNorm(hidden_size, eps=layernorm_eps)
+        self.mlp = BasicTEMLP(
+            hidden_size=hidden_size,
+            ffn_hidden_size=ffn_hidden_size,
+        )
+        
+    def forward(self, 
+                x: torch.Tensor, 
+                attention_mask: torch.Tensor=None):
+        res = x
+        x = self.ln1(x)
+        
+        # Fused QKV projection
+        qkv = self.qkv_projection(x)
+        qkv = qkv.view(qkv.size(0), qkv.size(1), self.num_attention_heads, 3 * self.kv_channels)
+        q, k, v = torch.split(qkv, qkv.size(3) // 3, dim=3)
+        
+        x = self.attention(q, k, v, attention_mask)
+        x = self.projection(x)
+        x = self.dropout(x)
+        x = res + x
+        res = x
+        x = self.ln2(x)
+        x = self.mlp(x)
+        
+        return x + res
+
+
+# In[15]:
+
+
+basic_te_transformer = BasicTETransformerLayer(
+    hidden_size, 
+    ffn_hidden_size, 
+    num_attention_heads,
+)
+basic_te_transformer.to(dtype=dtype).cuda()
+utils.share_parameters_with_basic_te_model(basic_te_transformer, basic_transformer)
+
+
+# In[16]:
+
+
+torch.manual_seed(1234)
+y = basic_te_transformer(x, attention_mask=None)
+
+
+# In[19]:
+
+
+fprof_te = lambda: utils.speedometer_simple(
+    basic_te_transformer,
+    x,
+    dy,
+    forward_kwargs = { "attention_mask": None },
+)
+fprof_te()
+
+
+# In[20]:
+
+
+stat, text = profile(fprof_te)
+
+gr = profile2graph(stat)
+print(gr[0].to_text(fct_width=80))
+
+
+# In[13]:
+
+
+from torch.onnx import export
+export(basic_te_transformer, x, "te.onnx", verbose=True, input_names=["X"], output_names=["Y"])
+
+
+# ## Fused TE Modules
+# 
+# <div class="alert alert-info">
+# 
+# <b>Summary</b>
+#     
+# We optimize the example Transformer layer with TE modules for fused operations.
+# 
+# </div>
+# 
+# The `Linear` layer is enough to build any Transformer model and it enables usage of Transformer Engine even for very custom Transformers. However, having more knowledge about the model allows for additional optimizations like kernel fusion, increasing the achievable speedup.
+# 
+# Transformer Engine therefore provides coarser modules that span multiple layers:
+# 
+# * `LayerNormLinear`
+# * `LayerNormMLP`
+# * `TransformerLayer`
+# 
+# Building a third iteration of our Transformer layer with `LayerNormLinear` and `LayerNormMLP`:
+
+# In[ ]:
+
+
+class FusedTETransformerLayer(torch.nn.Module):
+    def __init__(self,
+                 hidden_size: int,
+                 ffn_hidden_size: int,
+                 num_attention_heads: int,
+                 layernorm_eps: int = 1e-5,
+                 attention_dropout: float = 0.1,
+                 hidden_dropout: float = 0.1):
+        super().__init__()
+        self.num_attention_heads = num_attention_heads
+        self.kv_channels = hidden_size // num_attention_heads
+        self.ln_qkv = te.LayerNormLinear(hidden_size, 3 * hidden_size, eps=layernorm_eps, bias=True)
+        self.attention = utils.DotProductAttention(
+            num_attention_heads=num_attention_heads,
+            kv_channels=self.kv_channels,
+            attention_dropout=attention_dropout,
+        )
+        self.projection = te.Linear(hidden_size, hidden_size, bias=True)
+        self.dropout = torch.nn.Dropout(hidden_dropout)
+        self.ln_mlp = te.LayerNormMLP(hidden_size, ffn_hidden_size, eps=layernorm_eps, bias=True)
+        
+        
+    def forward(self, 
+                x: torch.Tensor, 
+                attention_mask: torch.Tensor):
+        res = x
+        qkv = self.ln_qkv(x)
+        
+        # Split qkv into query, key and value
+        qkv = qkv.view(qkv.size(0), qkv.size(1), self.num_attention_heads, 3 * self.kv_channels)
+        q, k, v = torch.split(qkv, qkv.size(3) // 3, dim=3)
+        
+        x = self.attention(q, k, v, attention_mask)
+        x = self.projection(x)
+        x = self.dropout(x)
+        x = res + x
+        res = x
+        x = self.ln_mlp(x)
+        
+        return x + res
+
+
+# In[ ]:
+
+
+fused_te_transformer = FusedTETransformerLayer(hidden_size, ffn_hidden_size, num_attention_heads)
+fused_te_transformer.to(dtype=dtype).cuda()
+utils.share_parameters_with_fused_te_model(fused_te_transformer, basic_transformer)
+
+
+# In[ ]:
+
+
+torch.manual_seed(1234)
+y = fused_te_transformer(x, attention_mask=None)
+
+
+# In[ ]:
+
+
+utils.speedometer(
+    fused_te_transformer,
+    x,
+    dy,
+    forward_kwargs = { "attention_mask": None },
+)
+
+
+# Finally, the `TransformerLayer` module is convenient for creating standard Transformer architectures and it provides the highest degree of performance optimization:
+
+# In[ ]:
+
+
+te_transformer = te.TransformerLayer(hidden_size, ffn_hidden_size, num_attention_heads)
+te_transformer.to(dtype=dtype).cuda()
+utils.share_parameters_with_transformerlayer_te_model(te_transformer, basic_transformer)
+
+
+# In[ ]:
+
+
+torch.manual_seed(1234)
+y = te_transformer(x, attention_mask=None)
+
+
+# In[ ]:
+
+
+utils.speedometer(
+    te_transformer,
+    x,
+    dy,
+    forward_kwargs = { "attention_mask": None },
+)
+
+
+# ## Enabling FP8
+# 
+# <div class="alert alert-info">
+# 
+# <b>Summary</b>
+#     
+# We configure a TE module to perform compute in FP8.
+# 
+# </div>
+# 
+# Enabling FP8 support is very simple in Transformer Engine. We just need to wrap the modules within an [fp8_autocast](../api/pytorch.rst#transformer_engine.pytorch.fp8_autocast) context manager. Note that fp8_autocast should only be used to wrap the forward pass and must exit before starting a backward pass. See the [FP8 tutorial](fp8_primer.ipynb) for a detailed explanation of FP8 recipes and the supported options.
+
+# In[ ]:
+
+
+from transformer_engine.common.recipe import Format, DelayedScaling
+
+te_transformer = te.TransformerLayer(hidden_size, ffn_hidden_size, num_attention_heads)
+te_transformer.to(dtype=dtype).cuda()
+utils.share_parameters_with_transformerlayer_te_model(te_transformer, basic_transformer)
+
+fp8_format = Format.HYBRID
+fp8_recipe = DelayedScaling(fp8_format=fp8_format, amax_history_len=16, amax_compute_algo="max")
+torch.manual_seed(1234)
+with te.fp8_autocast(enabled=True, fp8_recipe=fp8_recipe):
+    y = te_transformer(x, attention_mask=None)
+
+
+# In[ ]:
+
+
+utils.speedometer(
+    te_transformer,
+    x,
+    dy,
+    forward_kwargs = { "attention_mask": None },
+    fp8_autocast_kwargs = { "enabled": True, "fp8_recipe": fp8_recipe },
+)
+
+
---
+++ 2023/te/short_example.py
@@ -0,0 +1,268 @@
+import os
+import pprint
+import warnings
+import torch
+from torch.onnx import export as torch_export
+
+
+def create_fp8_recipe():
+    from transformer_engine.common import recipe
+    return recipe.DelayedScaling(margin=0, interval=1, fp8_format=recipe.Format.E4M3)
+
+
+def te_export(
+        model: torch.nn.Module,
+        inp: torch.Tensor,
+        fname: str,
+        use_fp8: bool=True,
+        opset: int=15,
+        input_names: list=["input"],
+        output_names: list=["output"],
+        ONNX_FILES_DIR="."):
+    """
+    Export to ONNX
+    Taken from the unit test of TransformerEngine.
+    """
+    import transformer_engine.pytorch as te
+    fp8_recipe = create_fp8_recipe()
+
+    with (torch.inference_mode(),
+          te.fp8_autocast(enabled=use_fp8, fp8_recipe=fp8_recipe),
+          warnings.catch_warnings()):
+        warnings.filterwarnings(
+            action='ignore',
+            category=torch.jit.TracerWarning,
+            module=r'.*'
+        )
+
+        model.cuda().eval()
+        os.makedirs(ONNX_FILES_DIR, exist_ok=True)
+        fname = os.path.join(ONNX_FILES_DIR, fname)
+        torch.onnx.export(model,
+                          inp if isinstance(inp, list) or isinstance(inp, tuple) else (inp,),
+                          fname,
+                          verbose=False,
+                          opset_version=opset,
+                          input_names=input_names,
+                          output_names=output_names,
+                          do_constant_folding=True,
+                          operator_export_type=torch.onnx.OperatorExportTypes.ONNX_FALLTHROUGH)
+
+
+class BasicLayer(torch.nn.Module):
+    def __init__(self, hidden_size: int, layernorm_eps: int = 1e-5):
+        super().__init__()
+        self.linear1 = torch.nn.Linear(hidden_size, 1, bias=True)
+        
+    def forward(self, x):
+        x1 = self.linear1(x)
+        return torch.nn.functional.gelu(x1, approximate='tanh')
+
+
+dtype = torch.float16
+hidden_size = 128
+sequence_length = 64
+x = torch.rand(sequence_length, hidden_size).cuda().to(dtype=dtype)
+y = torch.rand(sequence_length, 1).cuda().to(dtype=dtype)
+
+layer = BasicLayer(hidden_size)
+layer.to(dtype=dtype).cuda()
+
+
+
+timing_iters = 10
+
+def train_torch(layer, x, y, timing_iters=10):
+    for _ in range(timing_iters):
+        output = layer(x)
+        loss = ((output - y) ** 2).sum(axis=0)
+        layer.zero_grad()
+        loss.backward()
+
+print("training BasicLayer")
+train_torch(layer, x, y)
+print("done.")
+
+print("TransformerEngine")
+import transformer_engine.pytorch as te
+
+
+class TeBasicLayer(torch.nn.Module):
+    def __init__(self, hidden_size: int, layernorm_eps: int = 1e-5):
+        super().__init__()
+        self.linear1 = te.Linear(hidden_size, 1, bias=True)
+        
+    def forward(self, x):
+        x1 = self.linear1(x)
+        return torch.nn.functional.gelu(x1, approximate='tanh')
+
+
+layer_te = TeBasicLayer(hidden_size)
+layer_te.to(dtype=dtype).cuda()
+
+
+def train_te(layer, x, y, timing_iters=10, fp8=False):
+    fp8_autocast_kwargs = { "enabled": fp8 }
+    for _ in range(timing_iters):
+        with te.fp8_autocast(**fp8_autocast_kwargs):
+            output = layer(x)
+            loss = ((output - y) ** 2).sum(axis=0)
+        layer.zero_grad()
+        loss.backward()
+
+print("training TeBasicLayer")
+train_te(layer_te, x, y)
+print("done.")
+
+print("#################################################")
+print("to_onnx - torch")
+#state_dict = layer.state_dict(keep_vars=True)
+state_dict = layer.state_dict()
+for k, v in state_dict.items():
+    print("TO", k, v.shape, v.dtype)
+
+torch_export(layer, x, "basic.torch.onnx", verbose=False, input_names=["X"], output_names=["Y"])
+
+print("#################################################")
+state_dict = layer_te.state_dict(keep_vars=True)
+state_dict = layer_te.state_dict()
+for k, v in state_dict.items():
+     print("TE:", k, type(v), v.dtype)
+print("to_onnx - torch - TransformerEngine")
+# This does not work when get_extra_state is overriden.
+# In TransformerEngine, it returns None when fp8 is disabled.
+# But even though, it says only tensors can be traced in
+# torch._C._create_graph_by_tracing.
+te_export(layer_te, x, "basic.te.onnx", input_names=["X"], output_names=["Y"], use_fp8=False)
+print("to_onnx - torch - TransformerEngine - f8")
+try:
+    te_export(layer_te, x, "basic.te.fp8.onnx", input_names=["X"], output_names=["Y"], use_fp8=True)
+except AssertionError as e:
+    print(e)
+print("#################################################")
+
+
+if False:
+    from pyquickhelper.pycode.profiling import profile
+    from pyquickhelper.pycode.profiling import profile2graph
+
+    stat, text = profile(lambda: train_torch(layer, x, y))
+    gr = profile2graph(stat)
+    print(gr[0].to_text(fct_width=80))
+
+    stat, text = profile(lambda: train_te(layer_te, x, y))
+    gr = profile2graph(stat)
+    print(gr[0].to_text(fct_width=80))
+
+
+print("Same with scaling")
+
+
+class BasicLayerSoft(torch.nn.Module):
+    def __init__(self, hidden_size: int, layernorm_eps: int = 1e-5):
+        super().__init__()
+        self.linear1 = torch.nn.Linear(hidden_size, 1, bias=True)
+        self.soft = torch.nn.Softmax(dim=1)
+        
+    def forward(self, x):
+        x1 = self.linear1(x)
+        y = self.soft(x1)
+        return y
+
+
+layer_norm = BasicLayerSoft(hidden_size)
+layer_norm.to(dtype=dtype).cuda()
+layer_norm(x).shape
+
+print("training BasicLayerSoft")
+train_torch(layer_norm, x, y)
+print("done.")
+
+
+
+class TeBasicLayerNorm(torch.nn.Module):
+    def __init__(self, hidden_size: int, layernorm_eps: int = 1e-5):
+        super().__init__()
+        self.linear1 = te.Linear(hidden_size, 1, bias=True)
+        self.norm = te.LayerNormLinear(1, 1)
+        
+    def forward(self, x):
+        x1 = self.linear1(x)
+        y = self.norm(x1)
+        return y
+
+
+print("TransformerEngine: TeBasicLayerNorm")
+te_layer_norm = TeBasicLayerNorm(hidden_size)
+te_layer_norm.to(dtype=dtype).cuda()
+te_layer_norm(x).shape
+
+
+print("training TeBasicLayerNorm")
+train_te(te_layer_norm, x, y)
+print("done.")
+
+
+print("#################################################")
+print("to_onnx - torch")
+from torch.onnx import export
+torch_export(layer_norm, x, "norm.torch.onnx", verbose=False, input_names=["X"], output_names=["Y"])
+
+print("#################################################")
+print("to_onnx - torch - TransformerEngine")
+
+# Once get_extra_state is removed. It fails here in _trace_and_get_graph_from_model (C++ issue).
+# Gemm is used in the normalization. It goes through one of the cpp extension.
+# A custom gemm function is called with a preallocated buffer: gemm(input, output) -> None.
+# This call is made in a torch function and then encapsulated in a torch module.
+# My guess it breaks the tracing somehow.
+# https://github.com/microsoft/onnxruntime-extensions/blob/main/tutorials/pytorch_custom_ops_tutorial.ipynb
+
+te_export(te_layer_norm, x, "norm.te.onnx", input_names=["X"], output_names=["Y"], use_fp8=False)
+te_export(te_layer_norm, x, "norm.te.fp8.onnx", input_names=["X"], output_names=["Y"], use_fp8=False)
+
+
+
+if False:
+    stat, text = profile(lambda: train_te(te_layer_norm, x, y))
+    gr = profile2graph(stat)
+    print(gr[0].to_text(fct_width=80))
+
+    global_in = True
+
+    def tracefunc(frame, event, arg, indent=[0]):
+        global global_in
+        if event == "call":
+            if frame.f_code.co_name == "tracefunc":
+                global_in = False
+            if global_in:
+                filename = frame.f_code.co_filename
+                filename = filename.split("site-packages")[-1]
+                filename = filename.split("python3.10")[-1]
+                line = frame.f_code.co_firstlineno
+                print("-" * indent[0] + f"> {frame.f_code.co_name!r} - {filename}:{line}")
+                indent[0] += 2
+        elif event == "return":
+            indent[0] -= 2
+            if global_in:
+                print("-" * indent[0] + f"< {frame.f_code.co_name!r}")
+            if frame.f_code.co_name == "tracefunc":
+                global_in = True
+        return tracefunc
+
+    import sys
+    f = sys.getprofile()
+    sys.setprofile(tracefunc)
+    train_te(te_layer_norm, x, y, 2)
+    sys.setprofile(f)
+    f = sys.getprofile()
+    sys.setprofile(tracefunc)
+    train_te(te_layer_norm, x, y, 2, fp8=True)
+    sys.setprofile(f)
+    sys.setprofile(f)
+    train_te(te_layer_norm, x, y, 2, fp8=True)
+
+
+
+
+
